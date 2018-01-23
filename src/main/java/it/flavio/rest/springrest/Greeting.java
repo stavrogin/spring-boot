b923a590b8d8057c07c773d8069ea370a4b4bda9
ynap username <ynap email>
2018-01-23 15:50:48 +0100

    first commit

---
+++ src/main/java/it/flavio/rest/springrest/Greeting.java
@@ -0,0 +1,21 @@
+package it.flavio.rest.springrest;
+
+public class Greeting {
+	
+	private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
