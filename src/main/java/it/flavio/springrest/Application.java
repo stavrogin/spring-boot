d5bfe96b0e455dc9309411f07465a53bbd4e8f6d
flavio <flavio.ricci82@gmail.com>
2018-01-27 15:04:23 +0100

    tomcat 8 deployment configurations

--- src/main/java/it/flavio/springrest/Application.java
+++ src/main/java/it/flavio/springrest/Application.java
@@ -1,13 +1,25 @@
 package it.flavio.springrest;
 
 import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * extends SpringBootServletInitializer and configure method: necesary to deploy on tomcat
+ * @author flavio
+ */
 @SpringBootApplication
 @MapperScan("it.flavio.springrest.mybatis.mapper")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
